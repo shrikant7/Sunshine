ac242e7c16999ba3380fc4c738a54b651a76b0d9
shrikant <shrikantlnmiit@gmail.com>
2016-06-26 19:22:22 +0530

    Added a dummy weather data list to ListView.

--- app/src/main/java/com/example/user/sunshine/MainActivity.java
+++ app/src/main/java/com/example/user/sunshine/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.user.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayAdapter<String> mForecastAdapter;
+    ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* dummy weather forecast data */
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Wed - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - HELP TRAPPED IN WEATHERSTATION",
+                "Sun - Sunny - 80/68"
+        };
+
+        //setting list with dummy data array.
+        List<String> weekForecast = new LinkedList<String>(Arrays.asList(forecastArray));
+
+        //Adapter to view sourse list to listView.
+        mForecastAdapter = new ArrayAdapter<String>(this,R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+        mListView = (ListView) findViewById(R.id.listview_forecast);
+        mListView.setAdapter(mForecastAdapter);
     }
 }
